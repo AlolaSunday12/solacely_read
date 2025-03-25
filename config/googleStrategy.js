0ad30ae87a1cdfa551c1637e7e6bda729e710e8c
AlolaSunday12 <alolasj@gmail.com>
2025-03-25 21:03:22 +0100

    update on google strategy

--- config/googleStrategy.js
+++ config/googleStrategy.js
@@ -1,24 +1,14 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require('./keys');
+//const keys = require('./keys');
 const User = require('../models/user'); // Import the User model
 
-// Serialize the user into the session (store the user ID in the session)
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-// Deserialize the user from the session (retrieve user by ID)
-passport.deserializeUser((id, done) => {
-    User.findByPk(id).then((user) => {
-        done(null, user);
-    }).catch((err) => console.error('Error deserializing user:', err));
-});
+require('dotenv').config();
 
 // Google OAuth Strategy for Passport
 passport.use(new GoogleStrategy({
-    clientID: keys.google.clientID,
-    clientSecret: keys.google.clientSecret,
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: 'http://localhost:5000/auth/google/redirect',
     scope: ['profile', 'email']
 }, (accessToken, refreshToken, profile, done) => {

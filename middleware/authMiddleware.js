60a5db4f854889cb45121beab80768088905fd74
AlolaSunday12 <alolasj@gmail.com>
2025-03-28 22:19:06 +0100

    update on authentication

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,11 @@
-// Auth check middleware to protect routes
 const authCheck = (req, res, next) => {
+    console.log("AUTH CHECK - REQ.USER:", req.user);
+    console.log("AUTH CHECK - IS AUTHENTICATED:", req.isAuthenticated());
+
     if (!req.isAuthenticated() || !req.user) {
-        return res.redirect('/auth/login'); // Redirect to login if user is not authenticated
+        return res.status(401).json({ message: "Unauthorized. Please log in." });
     }
+
     next();
 };
 

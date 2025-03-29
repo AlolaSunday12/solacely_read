0fb1354eeda58f0811cef960989f6e2c258ca300
AlolaSunday12 <alolasj@gmail.com>
2025-03-29 23:53:56 +0100

    update on controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const User = require('../models/user');
+//const bcrypt = require('bcrypt');
+
+
+
 
 exports.getUserProfile = async (req, res) => {
     try {
@@ -32,5 +36,37 @@ exports.getAllUsers = async (req, res) => {
     } catch (error) {
         console.error('Error fetching users:', error);
         res.status(500).json({ message: 'Server error' });
+    }
+}
+
+  //  const User = require('../models/User');
+
+  exports.updateProfile = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const { username, email } = req.body;
+        let user = await User.findByPk(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        let thumbnailPath;
+        const basePath = `${req.protocol}://${req.get('host')}/uploads/`;
+
+        if (req.file) {
+            thumbnailPath = `${basePath}${req.file.filename}`;
+        }
+
+        // Update fields
+        if (username) user.username = username;
+        if (email) user.email = email;
+        if (thumbnailPath) user.thumbnail = thumbnailPath;
+
+        await user.save();
+
+        res.json({ message: "Profile updated successfully", user });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
     }
 };

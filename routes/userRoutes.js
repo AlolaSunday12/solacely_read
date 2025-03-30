11f0dbc8b7e3a5e1e452301ecba8396f13deb06f
AlolaSunday12 <alolasj@gmail.com>
2025-03-30 16:33:08 +0100

    update on user Route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,17 +3,16 @@ const passport = require('passport');
 const userController = require('../controllers/userController');
 const authCheck = require('../middleware/authMiddleware')
 const upload = require('../middleware/uploadMiddleware'); 
-
 const path = require('path');
 
 const router = express.Router();
 
-//const upload = multer({ dest: 'Public/uploads/' });
 
 router.put('/updateProfile', authCheck, upload.single('thumbnail'), userController.updateProfile); // ✅ Use userController.updateProfile
 
 router.get('/getUser', authCheck, userController.getUserProfile);
 router.get('/getAllUsers', authCheck, userController.getAllUsers);
+router.delete('/deleteUser/:id', authCheck, userController.deleteUser);
 
 // Protect the home route by ensuring the user is authenticated
 router.get('/', authCheck, (req, res) => {

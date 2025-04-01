b5c45403496c4996b62de57e14610534098ebb6a
AlolaSunday12 <alolasj@gmail.com>
2025-04-01 23:35:05 +0100

    update on strategy

--- config/facebookStrategy.js
+++ config/facebookStrategy.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
-const User = require('../models/user');
+const { User } = require('../models/user');
 //const keys = require('./keys');
 
 require('dotenv').config();

c75fa5895c734aabfdffc9df0154a2f38628890a
AlolaSunday12 <alolasj@gmail.com>
2025-04-02 23:19:31 +0100

    update on auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const passport = require('passport');
 const { User } = require('../models/user');
 const bcrypt = require('bcryptjs')
-//const { sendOTPEmail } = require('../config/otp');
 const crypto = require('crypto');
 const authCheck = require('../middleware/authMiddleware')
 const {sendOTPEmail} = require('../utils/sendEmail')

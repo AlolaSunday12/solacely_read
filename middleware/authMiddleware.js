58f86cbcc1988a760be52e74e866e29d0da504bc
AlolaSunday12 <alolasj@gmail.com>
2025-04-18 12:22:43 +0100

    update on auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -9,4 +9,17 @@ const authCheck = (req, res, next) => {
     next();
 };
 
+/*
+const authorizeAdmin = (req, res, next) => {
+    if (!req.user || !req.user.isAdmin) {
+      return res.status(403).json({ message: 'Access denied' });
+    }
+    next();
+  };const authorizeAdmin = (req, res, next) => {
+    if (!req.user || !req.user.isAdmin) {
+      return res.status(403).json({ message: 'Access denied' });
+    }
+    next();
+  };
+*/
 module.exports = authCheck;
